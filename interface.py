6a3d9dce09118c887c26e670ac3fb3f24c30833a
Memogoz <gonzalezg.guillermo@edu.uag.mx>
2024-11-19 21:49:33 -0600

    Ajuste de variables y limpieza de código

--- interface.py
+++ interface.py
@@ -4,9 +4,11 @@ from PyQt5.QtCore import QThread, pyqtSignal
 
 import multiprocessing
 import time
-import random #--- temp
+import random 
 
 from scrapper import scrape_books
+
+
 
 class WorkerThread(QThread):
 
@@ -14,13 +16,10 @@ class WorkerThread(QThread):
         super().__init__()
         self.category = category
 
-    # Señal para enviar el progreso actualizado a la GUI
     update_progress = pyqtSignal(int)
     update_text = pyqtSignal(str)
     
     def run(self):
-        
-        
 
         q = multiprocessing.Queue()
         p = multiprocessing.Process(target=scrape_books,args=(q,self.category))
@@ -38,12 +37,10 @@ class WorkerThread(QThread):
                 totalProducts = rsp
                 continue
             
-
             self.update_progress.emit((prodCount*100)//totalProducts)
             self.update_text.emit(rsp)
 
             prodCount += 1
-            
 
         print('terminado')
         p.join()
@@ -62,50 +59,49 @@ class MainWindow(QMainWindow):
         self.layout = QVBoxLayout()
 
         self.layoutTop = QHBoxLayout()
-        self.layoutShops = QHBoxLayout()
+        self.layoutHeader = QHBoxLayout()
         self.layoutContent = QHBoxLayout()
 
         self.layout1 = QVBoxLayout()
         self.layout2 = QVBoxLayout()
         self.layout3 = QVBoxLayout()
 
-        # Top Layout
         self.layoutTop.addWidget(QLabel('Book Scrapper           '))
         self.button = QPushButton('Search')
         self.button.clicked.connect(self.start_task)
         self.layoutTop.addWidget(self.button)
         
         self.LabelA = QLabel('A')
-        self.layoutShops.addWidget(self.LabelA)
-        self.progressBarAmazon = QProgressBar()
-        self.layoutShops.addWidget(self.progressBarAmazon)
+        self.layoutHeader.addWidget(self.LabelA)
+        self.progressBarA = QProgressBar()
+        self.layoutHeader.addWidget(self.progressBarA)
         self.LabelB = QLabel('              B')
-        self.layoutShops.addWidget(self.LabelB)
-        self.progressBarAliexpress = QProgressBar()
-        self.layoutShops.addWidget(self.progressBarAliexpress)
+        self.layoutHeader.addWidget(self.LabelB)
+        self.progressBarB = QProgressBar()
+        self.layoutHeader.addWidget(self.progressBarB)
         self.LabelC = QLabel('        C')
-        self.layoutShops.addWidget(self.LabelC)
-        self.progressBarMercado = QProgressBar()
-        self.layoutShops.addWidget(self.progressBarMercado)
+        self.layoutHeader.addWidget(self.LabelC)
+        self.progressBarC = QProgressBar()
+        self.layoutHeader.addWidget(self.progressBarC)
 
-        self.productsBoxAmazon = QTextEdit()
-        self.productsBoxAmazon.setReadOnly(True)  # Evitar que el usuario edite el texto
-        self.layout1.addWidget(self.productsBoxAmazon)
+        self.productsBoxA = QTextEdit()
+        self.productsBoxA.setReadOnly(True)  # Evitar que el usuario edite el texto
+        self.layout1.addWidget(self.productsBoxA)
 
-        self.productsBoxAliexpress = QTextEdit()
-        self.productsBoxAliexpress.setReadOnly(True)  
-        self.layout2.addWidget(self.productsBoxAliexpress)
+        self.productsBoxB = QTextEdit()
+        self.productsBoxB.setReadOnly(True)  
+        self.layout2.addWidget(self.productsBoxB)
 
-        self.productsBoxMercado = QTextEdit()
-        self.productsBoxMercado.setReadOnly(True)  
-        self.layout3.addWidget(self.productsBoxMercado)
+        self.productsBoxC = QTextEdit()
+        self.productsBoxC.setReadOnly(True)  
+        self.layout3.addWidget(self.productsBoxC)
 
         self.layoutContent.addLayout(self.layout1)
         self.layoutContent.addLayout(self.layout2)
         self.layoutContent.addLayout(self.layout3)
 
         self.layout.addLayout(self.layoutTop)
-        self.layout.addLayout(self.layoutShops)
+        self.layout.addLayout(self.layoutHeader)
         self.layout.addLayout(self.layoutContent)
 
         widget = QWidget()
@@ -127,53 +123,47 @@ class MainWindow(QMainWindow):
         self.update_title_C(chosenCategory3)
 
         self.thread1 = WorkerThread(chosenCategory1)
-        # Conectar las señales a métodos separados
-        self.thread1.update_progress.connect(self.update_progress_amazon)
-        self.thread1.update_text.connect(self.update_box_amazon)
-        # Reactivar el botón cuando el hilo finalice
+        self.thread1.update_progress.connect(self.update_progress_A)
+        self.thread1.update_text.connect(self.update_box_A)
         self.thread1.finished.connect(lambda: self.button.setEnabled(True))
 
         self.thread2 = WorkerThread(chosenCategory2)
-        # Conectar las señales a métodos separados
-        self.thread2.update_progress.connect(self.update_progress_aliexpress)
-        self.thread2.update_text.connect(self.update_box_aliexpress)
+        self.thread2.update_progress.connect(self.update_progress_B)
+        self.thread2.update_text.connect(self.update_box_B)
 
         self.thread3 = WorkerThread(chosenCategory3)
-        # Conectar las señales a métodos separados
-        self.thread3.update_progress.connect(self.update_progress_mercado)
-        self.thread3.update_text.connect(self.update_box_mercado)
+        self.thread3.update_progress.connect(self.update_progress_C)
+        self.thread3.update_text.connect(self.update_box_C)
 
-
-        # Iniciar los hilos
         self.thread1.start()
         self.thread2.start()
         self.thread3.start()
     
 
 
-    def update_progress_amazon(self, value):
-        self.progressBarAmazon.setValue(value)
+    def update_progress_A(self, value):
+        self.progressBarA.setValue(value)
     
-    def update_box_amazon(self, message):
-        self.productsBoxAmazon.append(message)
+    def update_box_A(self, message):
+        self.productsBoxA.append(message)
 
     def update_title_A(self, title):
         self.LabelA.setText(title)
 
-    def update_progress_aliexpress(self, value):
-        self.progressBarAliexpress.setValue(value)
+    def update_progress_B(self, value):
+        self.progressBarB.setValue(value)
     
-    def update_box_aliexpress(self, message):
-        self.productsBoxAliexpress.append(message)
+    def update_box_B(self, message):
+        self.productsBoxB.append(message)
 
     def update_title_B(self, title):
         self.LabelB.setText(title)
 
-    def update_progress_mercado(self, value):
-        self.progressBarMercado.setValue(value)
+    def update_progress_C(self, value):
+        self.progressBarC.setValue(value)
     
-    def update_box_mercado(self, message):
-        self.productsBoxMercado.append(message)
+    def update_box_C(self, message):
+        self.productsBoxC.append(message)
 
     def update_title_C(self, title):
         self.LabelC.setText(title)
--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import random
 
 
 def scrape_books(queue,chosenCategoty):
@@ -9,7 +8,6 @@ def scrape_books(queue,chosenCategoty):
     r = requests.get('https://books.toscrape.com/catalogue/category/books/'+chosenCategoty+'/index.html')
     print(r) #HTTP status code
     
-    # Parsing the HTML
     soup = BeautifulSoup(r.content, 'html.parser')
 
     s = soup.find('section')
@@ -33,42 +31,5 @@ def scrape_books(queue,chosenCategoty):
     else:
         print("No se encontró el contenido deseado.")
 
-
-
     for book in book_list:
         queue.put(book)
-
-
-
-
-
-'''
-    example_scrape = [4,'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras congue mattis lorem, sed tincidunt nunc pharetra et.', 'Curabitur non laoreet ante. Integer at tempus magna. Nunc vel fringilla nisi. Sed ut lacinia velit.', 'Sed massa tortor, condimentum in tempus sit amet, elementum sit amet dolor.', 'Nullam ac purus egestas, gravida quam vel, fermentum velit.', None]
-
-    for product in example_scrape:
-        queue.put(product)
-'''
-
-def test_srapping():
-    r = requests.get('https://webscraper.io/test-sites/e-commerce/static/computers/laptops/')
-    print(r) #HTTP status code
-
-    # Parsing the HTML
-    soup = BeautifulSoup(r.content, 'html.parser')
-    #print(soup.prettify())
-
-    print(soup.title)
-
-    s = soup.find('div', class_='container test-site')
-    #print(s)
-
-    lista = []
-
-    if s is not None:
-        content = s.find_all('h4')
-        for paragraph in content:
-            lista.append(paragraph.get_text(strip=True))  # strip=True elimina espacios en blanco extra
-    else:
-        print("No se encontró el contenido deseado.")
-
-    print(lista)
